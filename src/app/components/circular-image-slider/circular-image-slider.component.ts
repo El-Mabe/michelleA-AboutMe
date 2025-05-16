6336e0acd523db7efcabb7ce47a07cc107d69625
Julián David Mabesoy Jimenéz <julian.mabesoyjimenez@mercadolibre.com.co>
2025-05-16 13:47:38 -0500

    se pudo organizar el slider de la izquierda con el comportamiento esperado

--- src/app/components/circular-image-slider/circular-image-slider.component.ts
+++ src/app/components/circular-image-slider/circular-image-slider.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from '@angular/core';
- import { trigger, state, style, animate, transition } from '@angular/animations';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-circular-image-slider',
@@ -22,15 +22,15 @@ export class CircularImageSliderComponent {
   @Input() captions: string[] = [];
   @Input() title: string = '';
   @ViewChild('circularContainer') containerRef!: ElementRef;
-  
+
   activeIndex: number = 0;
   radius: number = 0;
   containerWidth: number = 0;
   containerHeight: number = 0;
   autoplayInterval: any;
-  
-  constructor() {}
-  
+
+  constructor() { }
+
   ngAfterViewInit() {
     // Calculate container dimensions
     if (this.containerRef && this.containerRef.nativeElement) {
@@ -39,10 +39,10 @@ export class CircularImageSliderComponent {
       // this.radius = Math.min(this.containerWidth, this.containerHeight) * 0.35;
       this.radius = Math.min(this.containerWidth, this.containerHeight) * 0.5;
     }
-    
+
     // Start autoplay
     // this.startAutoplay();
-    
+
     // Handle window resize
     window.addEventListener('resize', this.handleResize);
   }
@@ -52,28 +52,36 @@ export class CircularImageSliderComponent {
     // this.stopAutoplay();
     // this.startAutoplay();
   }
-  
+
   getImagePosition(index: number): string {
     if (!this.images.length) return '';
-    
+
     const totalImages = this.images.length;
+    // let angle = ((index - this.activeIndex) / totalImages) * 2 * Math.PI - Math.PI;
+    // if (angle < 0) angle += 2 * Math.PI;
+
+
+    // const x = Math.sin(angle) * this.radius * 1.3; // usas cos aquí porque ya giraste el sistema
+    // const y = Math.cos(angle) * this.radius;       // y ahora sin es y
+
+
     let angle = ((index - this.activeIndex) / totalImages) * 2 * Math.PI;
-    if (angle < 0) angle += 2 * Math.PI;
-    
-    // const x = Math.sin(angle) * this.radius * 2;
-    // const x = Math.sin(angle) * this.radius;
-    const x = Math.sin(angle) * this.radius * 1.3;
-    // const y = -Math.cos(angle) * this.radius * 1.2; // Flatten the circle to make it more oval
-    const y = -Math.cos(angle) * this.radius; // Flatten the circle to make it more oval
-    
-    return `translate(${x}px, ${y}px)`;
+
+    const x = Math.cos(angle) * this.radius * 1.3; // -cos → derecha
+    const y = Math.sin(angle) * this.radius * 0.8;
+
+
+    const scale = index === this.activeIndex ? ' scale(1.5)' : '';
+
+    return `translate(${x}px, ${y}px)${scale}`;
+    // return `translate(${x}px, ${y}px)`;
   }
-  
+
   ngOnDestroy() {
     // this.stopAutoplay();
     window.removeEventListener('resize', this.handleResize);
   }
-  
+
   handleResize = () => {
     if (this.containerRef && this.containerRef.nativeElement) {
       this.containerWidth = this.containerRef.nativeElement.offsetWidth;
@@ -81,19 +89,19 @@ export class CircularImageSliderComponent {
       this.radius = Math.min(this.containerWidth, this.containerHeight) * 0.5;
     }
   }
-  
+
   // startAutoplay() {
   //   this.autoplayInterval = setInterval(() => {
   //     this.next();
   //   }, 5000);
   // }
-  
+
   // stopAutoplay() {
   //   if (this.autoplayInterval) {
   //     clearInterval(this.autoplayInterval);
   //   }
   // }
-  
 
 
- }
+
+}
