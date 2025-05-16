7e018846673e0b491387fc04e2cb2a8cb3f8237a
Julián David Mabesoy Jimenéz <julian.mabesoyjimenez@mercadolibre.com.co>
2025-05-16 14:42:54 -0500

    se añadieron los dos sliders

--- src/app/components/circular-image-slider/circular-image-slider.component.ts
+++ src/app/components/circular-image-slider/circular-image-slider.component.ts
@@ -7,24 +7,20 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   imports: [CommonModule],
   templateUrl: './circular-image-slider.component.html',
   styleUrl: './circular-image-slider.component.scss',
-  // animations: [
-  //   trigger('fadeIn', [
-  //     transition(':enter', [
-  //       style({ opacity: 0 }),
-  //       animate('300ms ease-in', style({ opacity: 1 }))
-  //     ])
-  //   ])
-  // ]
 })
 export class CircularImageSliderComponent {
 
   @Input() images: string[] = [];
+  @Input() imagesRight: string[] = [];
   @Input() captions: string[] = [];
   @Input() title: string = '';
-  @ViewChild('circularContainer') containerRef!: ElementRef;
+  @ViewChild('circularContainerLeft') containerRefL!: ElementRef;
+  @ViewChild('circularContainerRight') containerRefR!: ElementRef;
 
   activeIndex: number = 0;
+  activeIndexRight: number = 0;
   radius: number = 0;
+  radiusRight = 0;
   containerWidth: number = 0;
   containerHeight: number = 0;
   autoplayInterval: any;
@@ -33,15 +29,18 @@ export class CircularImageSliderComponent {
 
   ngAfterViewInit() {
     // Calculate container dimensions
-    if (this.containerRef && this.containerRef.nativeElement) {
-      this.containerWidth = this.containerRef.nativeElement.offsetWidth;
-      this.containerHeight = this.containerRef.nativeElement.offsetHeight;
-      // this.radius = Math.min(this.containerWidth, this.containerHeight) * 0.35;
+    if (this.containerRefL && this.containerRefL.nativeElement) {
+      this.containerWidth = this.containerRefL.nativeElement.offsetWidth;
+      this.containerHeight = this.containerRefL.nativeElement.offsetHeight;
       this.radius = Math.min(this.containerWidth, this.containerHeight) * 0.5;
     }
 
-    // Start autoplay
-    // this.startAutoplay();
+    if (this.containerRefR && this.containerRefR.nativeElement) {
+      this.containerWidth = this.containerRefR.nativeElement.offsetWidth;
+      this.containerHeight = this.containerRefR.nativeElement.offsetHeight;
+      this.radiusRight = Math.min(this.containerWidth, this.containerHeight) * 0.5;
+    }
+
 
     // Handle window resize
     window.addEventListener('resize', this.handleResize);
@@ -53,55 +52,65 @@ export class CircularImageSliderComponent {
     // this.startAutoplay();
   }
 
-  getImagePosition(index: number): string {
+  setActiveRight(index: number) {
+    this.activeIndexRight = index;
+  }
+  
+
+  getImagePositionL(index: number): string {
     if (!this.images.length) return '';
 
     const totalImages = this.images.length;
-    // let angle = ((index - this.activeIndex) / totalImages) * 2 * Math.PI - Math.PI;
-    // if (angle < 0) angle += 2 * Math.PI;
-
-
-    // const x = Math.sin(angle) * this.radius * 1.3; // usas cos aquí porque ya giraste el sistema
-    // const y = Math.cos(angle) * this.radius;       // y ahora sin es y
-
-
     let angle = ((index - this.activeIndex) / totalImages) * 2 * Math.PI;
-
-    const x = Math.cos(angle) * this.radius * 1.3; // -cos → derecha
+    const x = Math.cos(angle) * this.radius * 1.3; // cos → derecha
     const y = Math.sin(angle) * this.radius * 0.8;
-
-
     const scale = index === this.activeIndex ? ' scale(1.5)' : '';
 
     return `translate(${x}px, ${y}px)${scale}`;
-    // return `translate(${x}px, ${y}px)`;
   }
 
+  // getImagePositionR(index: number): string {
+  //   if (!this.imagesRight.length) return '';
+  //   const totalImages = this.imagesRight.length;
+  //   let angle = ((index - this.activeIndexRight) / totalImages) * 2 * Math.PI;
+  //   const x = -Math.cos(angle) * this.radiusRight * 1.3;
+  //   const y = Math.sin(angle) * this.radiusRight * 0.8;
+  //   const scale = index === this.activeIndexRight ? ' scale(1.4)' : '';
+  
+  //   return `translate(${x}px, ${y}px)${scale}`;
+  // }
+
+  getImagePositionR(index: number): string {
+    if (!this.imagesRight.length) return '';
+    
+    const totalImages = this.imagesRight.length;
+    const angle = ((index - this.activeIndexRight) / totalImages) * 2 * Math.PI;
+    const x = -Math.cos(angle) * this.radiusRight * 1.3;
+    const y = Math.sin(angle) * this.radiusRight * 0.8;
+    const scale = index === this.activeIndexRight ? ' scale(1.4)' : '';
+  
+    const offsetX = this.radiusRight * 2.5; // Ajusta este valor según lo "pegado" que lo quieras al borde
+  
+    return `translate(${x + offsetX}px, ${y}px)${scale}`;
+  }
+  
+
   ngOnDestroy() {
-    // this.stopAutoplay();
     window.removeEventListener('resize', this.handleResize);
   }
 
   handleResize = () => {
-    if (this.containerRef && this.containerRef.nativeElement) {
-      this.containerWidth = this.containerRef.nativeElement.offsetWidth;
-      this.containerHeight = this.containerRef.nativeElement.offsetHeight;
+    if (this.containerRefL && this.containerRefL.nativeElement) {
+      this.containerWidth = this.containerRefL.nativeElement.offsetWidth;
+      this.containerHeight = this.containerRefL.nativeElement.offsetHeight;
       this.radius = Math.min(this.containerWidth, this.containerHeight) * 0.5;
     }
+
+    if (this.containerRefR && this.containerRefR.nativeElement) {
+      this.containerWidth = this.containerRefR.nativeElement.offsetWidth;
+      this.containerHeight = this.containerRefR.nativeElement.offsetHeight;
+      this.radiusRight = Math.min(this.containerWidth, this.containerHeight) * 0.5;
+    }
+
   }
-
-  // startAutoplay() {
-  //   this.autoplayInterval = setInterval(() => {
-  //     this.next();
-  //   }, 5000);
-  // }
-
-  // stopAutoplay() {
-  //   if (this.autoplayInterval) {
-  //     clearInterval(this.autoplayInterval);
-  //   }
-  // }
-
-
-
 }
